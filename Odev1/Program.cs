8a93aff42fde45eba7afa5679bec2ee36f499992
MBurakT <mbtbrk11@outlook.com>
2023-12-26 22:52:19 +0300

    StringMethods

--- Odev1/Program.cs
+++ Odev1/Program.cs
@@ -1,54 +1,54 @@
 ﻿#region 1
-Console.Write("Numbers you will type: ");
-int.TryParse(Console.ReadLine(), out int n);
-Print1(n);
+// Console.Write("Numbers you will type: ");
+// int.TryParse(Console.ReadLine(), out int n);
+// Print1(n);
 
-static void Print1(int counter)
-{
-    if (counter > 0)
-    {
-        Console.Write("Input: ");
-        int.TryParse(Console.ReadLine(), out int input);
-        if (input % 2 == 0) Console.WriteLine(input);
-        Print1(counter - 1);
-    }
-}
+// static void Print1(int counter)
+// {
+//     if (counter > 0)
+//     {
+//         Console.Write("Input: ");
+//         int.TryParse(Console.ReadLine(), out int input);
+//         if (input % 2 == 0) Console.WriteLine(input);
+//         Print1(counter - 1);
+//     }
+// }
 #endregion
 
 #region 2
-Console.Write("Numbers you will type: ");
-int.TryParse(Console.ReadLine(), out int n);
-Console.Write("Control number: ");
-int.TryParse(Console.ReadLine(), out int m);
-Print2(n, m);
+// Console.Write("Numbers you will type: ");
+// int.TryParse(Console.ReadLine(), out int n);
+// Console.Write("Control number: ");
+// int.TryParse(Console.ReadLine(), out int m);
+// Print2(n, m);
 
-static void Print2(int counter, int control)
-{
-    if (counter > 0)
-    {
-        Console.Write("Input: ");
-        int.TryParse(Console.ReadLine(), out int input);
-        Print2(counter - 1, control);
-        if (input % control == 0) Console.WriteLine(input);
-    }
-}
+// static void Print2(int counter, int control)
+// {
+//     if (counter > 0)
+//     {
+//         Console.Write("Input: ");
+//         int.TryParse(Console.ReadLine(), out int input);
+//         Print2(counter - 1, control);
+//         if (input % control == 0) Console.WriteLine(input);
+//     }
+// }
 #endregion
 
 #region 3
-Console.Write("Words you will type: ");
-int.TryParse(Console.ReadLine(), out int n);
-Print3(n);
+// Console.Write("Words you will type: ");
+// int.TryParse(Console.ReadLine(), out int n);
+// Print3(n);
 
-static void Print3(int counter)
-{
-    if (counter > 0)
-    {
-        Console.Write("Input: ");
-        string input = Console.ReadLine() ?? "Invalid";
-        Print3(counter-1);
-        Console.Write(string.Format("{0} ", input));
-    }
-}
+// static void Print3(int counter)
+// {
+//     if (counter > 0)
+//     {
+//         Console.Write("Input: ");
+//         string input = Console.ReadLine() ?? "Invalid";
+//         Print3(counter-1);
+//         Console.Write(string.Format("{0} ", input));
+//     }
+// }
 #endregion
 
 #region 4
