fecbf805ef0e8da3d5ab7decf5a25996f7981459
MBurakT <mbtbrk11@outlook.com>
2023-12-27 18:17:13 +0300

    MethodRecursiveExtension

--- MethodRecursiveExtension/Program.cs
+++ MethodRecursiveExtension/Program.cs
@@ -32,3 +32,87 @@ int sayi = 5;
 Console.WriteLine(sayi.isEvenNumber());
 
 Console.WriteLine(ifade.GetFirstCharacter());
+
+public class Islemler
+{
+    // Expo(3,4) çağrıldığında ne döner?
+    // Expo(3,3) * 3;
+    // Expo(3,2) * 3 * 3;
+    // Expo(3,1) * 3 * 3 * 3;
+    // 3 * 3 * 3 * 3 = 3^4; 
+    public int Expo(int sayi, int us)
+    {
+        if (us < 2)
+            return sayi;
+
+        return Expo(sayi, us - 1) * sayi;
+    }
+    /*
+    FibonacciSerisi(4) çağrıldığında ne döner?
+    f(3)+f(2)
+    f(2)+f(1)+f(1)+f(0)
+    f(1)+f(0)+1+1+0
+    1+0+1+1=3
+    */
+    public int FibonacciSerisi(int i)
+    {
+        if (i == 0)
+            return 0;
+        else if (i == 1)
+            return 1;
+        else
+            return FibonacciSerisi(i - 1) + FibonacciSerisi(i - 2);
+    }
+    /*
+    factorial(4) = ?
+    factorial(3)*4
+    factorial(2)*3*4
+    factorial(1)*2*3*4
+    1*2*3*4 = 4! = 24    
+    */
+    public int Factorial(int n)
+    {
+        if (n <= 1)
+            return 1;
+        return Factorial(n - 1) * n;
+    }
+
+}
+public static class Extension
+{
+    public static bool CheckSpaces(this string param)
+    {
+        return param.Contains(" ");
+    }
+    public static string RemoveWhiteSpaces(this string param)
+    {
+        string[] dizi = param.Split(" ");
+        return string.Join("", dizi);
+    }
+    public static string MakeUpperCase(this string param)
+    {
+        return param.ToUpper();
+    }
+    public static string MakeLowerCase(this string param)
+    {
+        return param.ToLower();
+    }
+    public static int[] SortArray(this int[] param)
+    {
+        Array.Sort(param);
+        return param;
+    }
+    public static void EkranaYazdir(this int[] param)
+    {
+        foreach (var item in param)
+            Console.WriteLine(item);
+    }
+    public static bool isEvenNumber(this int param)
+    {
+        return param % 2 == 0;
+    }
+    public static string GetFirstCharacter(this string param)
+    {
+        return param.Substring(0, 1);
+    }
+}
