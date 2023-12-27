92aba3083a77f5d0319b7e452e864771c1e6a454
MBurakT <mbtbrk11@outlook.com>
2023-12-28 00:29:24 +0300

    Odev2/3

--- Odev2/3/Program3.cs
+++ Odev2/3/Program3.cs
@@ -5,7 +5,7 @@ using System.Collections;
 Console.Write("Text: ");
 string text = Console.ReadLine() ?? string.Empty;
 
-char[] vowels = text.ToLower().Where(p => "aeıioöuü".Contains(p)).Distinct().ToArray();
+char[] vowels = text.ToLower(CultureInfo.GetCultureInfo("tr-TR")).Where(p => "aeıioöuü".Contains(p)).Distinct().ToArray();
 Array.Sort(vowels);
 
 Console.WriteLine(string.Join(", ", vowels));
