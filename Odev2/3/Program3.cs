385750b3ae181f0d4a26b95ec3f17dd22bb10ac1
MBurakT <mbtbrk11@outlook.com>
2023-12-27 22:49:46 +0300

    Odev2/3

--- Odev2/3/Program3.cs
+++ Odev2/3/Program3.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+using System.Collections;
+
+Console.Write("Text: ");
+string text = Console.ReadLine() ?? string.Empty;
+
+char[] vowels = text.ToLower().Where(p => "aeıioöuü".Contains(p)).Distinct().ToArray();
+Array.Sort(vowels);
+
+Console.WriteLine(string.Join(", ", vowels));
