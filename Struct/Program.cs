e23760aa9a3f73c060f7b31f4abcc7ea8f7c7f83
MBurakT <mbtbrk11@outlook.com>
2023-12-28 18:13:38 +0300

    Struct

---
+++ Struct/Program.cs
@@ -0,0 +1,48 @@
+﻿Dikdortgen dikdortgen = new Dikdortgen();
+dikdortgen.Kisakenar = 3;
+dikdortgen.Uzunkenar = 4;
+
+Console.WriteLine("Class Alan Hesabı: {0}", dikdortgen.AlanHesapla());
+
+DikdortgenStruct dikdortgenStruct;
+dikdortgenStruct.Kisakenar = 3;
+dikdortgenStruct.Uzunkenar = 4;
+
+Console.WriteLine("Class Alan Hesabı: {0}", dikdortgenStruct.AlanHesapla());
+
+class Dikdortgen
+{
+    public int Kisakenar;
+    public int Uzunkenar;
+
+    public Dikdortgen() { }
+    public Dikdortgen(int kisakenar, int uzunkenar)
+    {
+        this.Kisakenar = kisakenar;
+        this.Uzunkenar = uzunkenar;
+    }
+
+    public long AlanHesapla()
+    {
+        return Kisakenar * Uzunkenar;
+    }
+}
+
+struct DikdortgenStruct
+{
+    public int Kisakenar;
+    public int Uzunkenar;
+
+    // Parametre almayan yani boş constructor tanımlanamıyor.
+
+    public DikdortgenStruct(int kisakenar, int uzunkenar)
+    {
+        this.Kisakenar = kisakenar;
+        this.Uzunkenar = uzunkenar;
+    }
+
+    public long AlanHesapla()
+    {
+        return Kisakenar * Uzunkenar;
+    }
+}
